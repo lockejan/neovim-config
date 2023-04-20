7ced7c427f4ac629174171204567414d89b4f124
Jan Schmitt <git@smittie.de>
2023-04-20 09:48:47 +0200

    fix: nvim-tree api changes
    * update plugins

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -3,8 +3,8 @@
 require("nvim-tree").setup({
   disable_netrw = false,
   hijack_netrw = false,
-  open_on_setup = false,
-  ignore_ft_on_setup = {},
+  -- open_on_setup = false,
+  -- ignore_ft_on_setup = {},
   -- auto_close = false,
   open_on_tab = false,
   hijack_cursor = false,
