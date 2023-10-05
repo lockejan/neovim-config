c2db9b11beaea37f933ef8b89c91bab9675e35ba
Jan Schmitt <git@smittie.de>
2023-10-05 21:56:58 +0200

    feat: migrate to newer indentline api

--- lua/config/indent-blankline.lua
+++ lua/config/indent-blankline.lua
@@ -1,11 +1,1 @@
--- vim.opt.list = true
--- vim.opt.listchars:append("space:⋅")
--- vim.opt.listchars:append("eol:↴")
-
-require("indent_blankline").setup({
-  -- space_char_blankline = " ",
-  show_current_context = true,
-  show_current_context_start = true,
-  buftype_exclude = { "terminal", "nofile" },
-  show_trailing_blankline_indent = false,
-})
+require("ibl").setup()
