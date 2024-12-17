75cb01a5153e7d63361360f1c2ba02e248d6f518
Jan Schmitt <git@smittie.de>
2024-12-17 13:42:49 +0100

    chore: update plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -91,7 +91,7 @@ require("lazy").setup({
   -- },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
+    branch = "main",
     dependencies = {
       { "github/copilot.vim" }, -- or github/copilot.vim
       { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
