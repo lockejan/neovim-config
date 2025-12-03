d83ed927fbd10072e01761dea57a372138bfdf35
Jan Schmitt <git@smittie.de>
2025-12-03 09:36:48 +0100

    chore: remove unused files

--- lua/config/trouble.lua
+++
@@ -1,9 +0,0 @@
-local map = require("utils").map
-require("trouble").setup({})
-
-map("n", "<leader>xx", "<cmd>TroubleToggle<cr>")
-map("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>")
-map("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>")
-map("n", "<leader>xl", "<cmd>Trouble loclist<cr>")
-map("n", "<leader>xq", "<cmd>Trouble quickfix<cr>")
-map("n", "gR", "<cmd>Trouble lsp_references<cr>")
