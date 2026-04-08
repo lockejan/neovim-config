6bf44afc57b525ba2dd9025ab46d593c6f1c969d
Jan Schmitt <jan.schmitt@wepa.eu>
2026-04-08 14:46:06 +0200

    feat: migrate treesitter config to main branch for Neovim 0.12
    The master branch of nvim-treesitter is obsolete and incompatible
    with Neovim 0.12. Migrate to the main branch rewrite which uses
    Neovim's built-in treesitter highlighting and a new plugin API.
    
    - Switch nvim-treesitter and textobjects to branch main
    - Replace nvim-treesitter.configs.setup with new API
    - Use vim.treesitter.start() for highlighting via FileType autocmd
    - Use vim.treesitter.foldexpr() for folding
    - Rewrite textobjects keymaps to new function-call style
    - Switch treesitter-context to nvim-treesitter org
    - Remove nvim-treesitter-refactor (archived, replaced by LSP)
    - Remove playground (replaced by built-in :InspectTree)
    - Configure standalone plugins (autotag, endwise, commentstring,
      rainbow-delimiters) independently

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,176 +1,109 @@
 -- Treesitter syntax highlighting and code manipulation plugins
 
 return {
-  -- Core treesitter
+  -- Core treesitter (main branch for Neovim 0.12+)
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-refactor",
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      "RRethy/nvim-treesitter-endwise",
-      "windwp/nvim-ts-autotag",
-      "JoosepAlviste/nvim-ts-context-commentstring",
-      "HiPhish/rainbow-delimiters.nvim",
-    },
+    lazy = false,
     config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "bash",
-          "c",
-          "css",
-          "dockerfile",
-          "gitcommit",
-          "html",
-          "lua",
-          "markdown",
-          "nix",
-          "python",
-          "rust",
-          "vim",
-          "yaml",
-        },
-        sync_install = true,
-        auto_install = true,
-        ignore_install = {},
-        modules = {},
+      require("nvim-treesitter").setup({})
 
-        highlight = {
-          enable = true,
-          disable = {},
-          additional_vim_regex_highlighting = false,
-        },
-        indent = {
-          enable = true,
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "gnn",
-            node_incremental = "grn",
-            scope_incremental = "grc",
-            node_decremental = "grm",
-          },
-        },
-        endwise = {
-          enable = true,
-        },
-        rainbow = {
-          enable = true,
-          extended_mode = true,
-          max_file_lines = 1000,
-        },
-        matchup = {
-          enable = true,
-        },
-        autotag = {
-          enable = true,
-        },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-              ["aC"] = "@conditional.outer",
-              ["iC"] = "@conditional.inner",
-              ["ad"] = "@comment.outer",
-              ["ae"] = "@block.outer",
-              ["am"] = "@call.outer",
-              ["al"] = "@loop.outer",
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true,
-            goto_next_start = {
-              ["]m"] = "@function.outer",
-              ["]]"] = "@class.outer",
-            },
-            goto_next_end = {
-              ["]M"] = "@function.outer",
-              ["]["] = "@class.outer",
-            },
-            goto_previous_start = {
-              ["[m"] = "@function.outer",
-              ["[["] = "@class.outer",
-            },
-            goto_previous_end = {
-              ["[M"] = "@function.outer",
-              ["[]"] = "@class.outer",
-            },
-          },
-          swap = {
-            enable = true,
-            swap_next = {
-              ["<leader>a"] = "@parameter.inner",
-            },
-            swap_previous = {
-              ["<leader>A"] = "@parameter.inner",
-            },
-          },
-          autopairs = { enable = true },
-          lsp_interop = {
-            enable = true,
-            border = "none",
-            peek_definition_code = {
-              ["<leader>df"] = "@function.outer",
-              ["<leader>dF"] = "@class.outer",
-            },
-            peek_type_definition_code = {
-              ["<leader>TF"] = "@class.outer",
-            },
-          },
-          refactor = {
-            highlight_definitions = {
-              enable = true,
-              clear_on_cursor_move = true,
-            },
-            highlight_current_scope = { enable = true },
-            smart_rename = {
-              enable = true,
-              keymaps = {
-                smart_rename = "grr",
-              },
-            },
-            navigation = {
-              enable = true,
-              keymaps = {
-                goto_definition = "gnd",
-                list_definitions = "gnD",
-                list_definitions_toc = "gO",
-                goto_next_usage = "<a-*>",
-                goto_previous_usage = "<a-#>",
-              },
-            },
-          },
-        },
+      -- Install parsers
+      require("nvim-treesitter").install({
+        "bash",
+        "c",
+        "css",
+        "dockerfile",
+        "gitcommit",
+        "html",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "nix",
+        "python",
+        "rust",
+        "vim",
+        "vimdoc",
+        "yaml",
       })
 
-      -- Custom parser configs
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.jsonc.filetype_to_parsername = "json"
-      parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
+      -- Enable treesitter highlighting and indentation for all filetypes
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          pcall(vim.treesitter.start)
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
 
-      -- Context commentstring setup
-      require("ts_context_commentstring").setup({})
-      vim.g.skip_ts_context_commentstring_module = true
+      -- Folding
+      vim.opt.foldmethod = "expr"
+      vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+      vim.opt.foldenable = false
+    end,
+  },
 
-      -- Folding setup
-      vim.cmd([[set foldmethod=expr]])
-      vim.cmd([[set foldexpr=nvim_treesitter#foldexpr()]])
-      vim.cmd([[set nofoldenable]])
+  -- Textobjects (main branch for Neovim 0.12+)
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    event = { "BufReadPost", "BufNewFile" },
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      local ts_select = require("nvim-treesitter-textobjects.select")
+      local ts_move = require("nvim-treesitter-textobjects.move")
+      local ts_swap = require("nvim-treesitter-textobjects.swap")
+
+      -- Selection keymaps
+      local select_maps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+        ["aC"] = "@conditional.outer",
+        ["iC"] = "@conditional.inner",
+        ["ad"] = "@comment.outer",
+        ["ae"] = "@block.outer",
+        ["am"] = "@call.outer",
+        ["al"] = "@loop.outer",
+      }
+      for key, query in pairs(select_maps) do
+        vim.keymap.set({ "x", "o" }, key, function()
+          ts_select.select_textobject(query, "textobjects")
+        end)
+      end
+
+      -- Move keymaps
+      local move_maps = {
+        { "]m", "goto_next_start", "@function.outer" },
+        { "]]", "goto_next_start", "@class.outer" },
+        { "]M", "goto_next_end", "@function.outer" },
+        { "][", "goto_next_end", "@class.outer" },
+        { "[m", "goto_previous_start", "@function.outer" },
+        { "[[", "goto_previous_start", "@class.outer" },
+        { "[M", "goto_previous_end", "@function.outer" },
+        { "[]", "goto_previous_end", "@class.outer" },
+      }
+      for _, map in ipairs(move_maps) do
+        vim.keymap.set({ "n", "x", "o" }, map[1], function()
+          ts_move[map[2]](map[3], "textobjects")
+        end)
+      end
+
+      -- Swap keymaps
+      vim.keymap.set("n", "<leader>a", function()
+        ts_swap.swap_next("@parameter.inner", "textobjects")
+      end)
+      vim.keymap.set("n", "<leader>A", function()
+        ts_swap.swap_previous("@parameter.inner", "textobjects")
+      end)
     end,
   },
 
   -- Sticky context header (shows current function/class at top)
   {
-    "romgrk/nvim-treesitter-context",
+    "nvim-treesitter/nvim-treesitter-context",
     event = { "BufReadPost", "BufNewFile" },
     opts = {
       enable = true,
@@ -178,17 +111,35 @@ return {
     },
   },
 
-  -- Treesitter playground for debugging
+  -- Endwise (auto-close do/end, if/end, etc.)
   {
-    "nvim-treesitter/playground",
-    cmd = { "TSPlaygroundToggle", "TSHighlightCapturesUnderCursor" },
+    "RRethy/nvim-treesitter-endwise",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-treesitter-endwise").setup({})
+    end,
   },
 
-  -- Additional dependencies (lazy loaded via treesitter)
-  { "nvim-treesitter/nvim-treesitter-refactor", lazy = true },
-  { "nvim-treesitter/nvim-treesitter-textobjects", lazy = true },
-  { "RRethy/nvim-treesitter-endwise", lazy = true },
-  { "windwp/nvim-ts-autotag", lazy = true },
-  { "JoosepAlviste/nvim-ts-context-commentstring", lazy = true },
-  { "HiPhish/rainbow-delimiters.nvim", lazy = true },
+  -- Auto-close and rename HTML tags
+  {
+    "windwp/nvim-ts-autotag",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {},
+  },
+
+  -- Context-aware commentstring
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    lazy = true,
+    config = function()
+      require("ts_context_commentstring").setup({})
+      vim.g.skip_ts_context_commentstring_module = true
+    end,
+  },
+
+  -- Rainbow delimiters
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+  },
 }
